188db073d0b40e35e29a6bb96e68c229a7b260b8
rainingchicken <154983686+rainingchicken@users.noreply.github.com>
2024-06-03 14:05:25 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// Part 1: Fizz Buzz
+// To begin, solve the following classic “Fizz Buzz” problem. There are a few different ways to do this - experiment with what you think is the most efficient. Once you have solved the problem, ask yourself if there could be another way; and if so, would it be better.
+// Accomplish the following:
+// Loop through all numbers from 1 to 100.
+// If a number is divisible by 3, log “Fizz.”
+// If a number is divisible by 5, log “Buzz.”
+// If a number is divisible by both 3 and 5, log “Fizz Buzz.”
+// If a number is not divisible by either 3 or 5, log the number.
+// Remember to commit your solution once it is working.
+
