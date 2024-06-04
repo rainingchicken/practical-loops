428d53036a24cce997674431e9bc81977c258b2a
rainingchicken <154983686+rainingchicken@users.noreply.github.com>
2024-06-04 15:46:03 -0700

    commit js solved part 2

--- script.js
+++ script.js
@@ -8,14 +8,54 @@
 // If a number is not divisible by either 3 or 5, log the number.
 // Remember to commit your solution once it is working.
 
-for (let num = 1; num <= 100; num++) {
-  if (num % 3 == 0 && num % 5 == 0) {
-    console.log("Fizz Buzz");
-  } else if (num % 5 == 0) {
-    console.log("Buzz");
-  } else if (num % 3 == 0) {
-    console.log("Frizz");
-  } else {
-    console.log(num);
+function fizzBuzz() {
+  for (let num = 1; num <= 100; num++) {
+    if (num % 3 == 0 && num % 5 == 0) {
+      console.log("Fizz Buzz");
+    } else if (num % 5 == 0) {
+      console.log("Buzz");
+    } else if (num % 3 == 0) {
+      console.log("Frizz");
+    } else {
+      console.log(num);
+    }
   }
 }
+
+fizzBuzz();
+
+// Part 2: Prime Time
+// Now we will move onto something slightly more complex.
+// Context: A prime number is any whole number greater than 1 that cannot be exactly divided by any whole number other than itself and 1. For example, the number 5 is prime because it cannot be divided by 4, 3, or 2; it can only be divided by itself (5) and 1. Similarly, the numbers 7 and 11 are prime. As numbers become larger, determining whether or not they are prime is increasingly difficult, but loops make this process relatively easy!
+// Write a script that accomplishes the following:
+// Declare an arbitrary number, n.
+// Create a loop that searches for the next prime number, starting at n and incrementing from there.
+// As soon as you find the prime number, log that number and exit the loop.
+// Continuing with the example above, if n is equal to 4, your loop should log 5. Similarly, if n is 5, it should log 7, and if n is 9, it should log 11. Test your loop with higher numbers and reference an online prime number table to determine the accuracy of your code.
+// Be careful! If you set n to a number too large, your loop could take a long time to process.
+let result;
+function findNextPrime(n) {
+  //if n is less than 3 then guaranteed prime
+  if (n <= 3) {
+    result = n;
+  } else {
+    //start index = 3
+    for (let index = 3; index < n; index++) {
+      //if n is divisble by something
+      if (n % index == 0) {
+        //increment n and go again
+        //havnt used recursion in so long yet ultra instinct
+        findNextPrime(++n);
+      } else {
+        //when n is not divisble by any num lesser than itself, return n cuz its prime
+        result = n;
+      }
+    }
+  }
+  return result;
+}
+//check function
+// for (let index = 0; index < 100; index++) {
+//   console.log(findNextPrime(index));
+// }
+//looks correct
